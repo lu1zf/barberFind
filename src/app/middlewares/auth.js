34d9d4bb198fa99fbe9916332ebd51ed9efb237b
Luiz Silva <luizsilva.ee@gmail.com>
2020-08-21 13:03:00 -0300

    criação de middlewares

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  return next();
+};
