32c399548c329f4ab7be6d1a77ad0f5d29498f8e
Luiz Silva <luizsilva.ee@gmail.com>
2020-08-26 20:16:50 -0300

    Implementada função de fazer update só quando autenticado

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,6 +1,9 @@
 import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
 
-export default (req, res, next) => {
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
@@ -9,5 +12,13 @@ export default (req, res, next) => {
 
   const [, token] = authHeader.split(' ');
 
-  return next();
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
 };
