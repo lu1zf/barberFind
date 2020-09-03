da5fb232707615aa6c7067059502b00d827a83c4
Luiz Silva <luizsilva.ee@gmail.com>
2020-09-03 18:26:49 -0300

    Configurado upload e URL de File

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -18,10 +19,8 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
-// routes.post('/files', upload.single('file'), (req, res) => {
-//   return res.json(req.file);
-// });
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
